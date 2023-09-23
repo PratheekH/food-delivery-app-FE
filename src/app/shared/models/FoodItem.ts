273db8221bfae9d2afa539f10dd063e88071bff0
PratheekH <pratheekharalike993@gmail.com>
2023-09-23 09:20:42 +0200

    first commit

---
+++ src/app/shared/models/FoodItem.ts
@@ -0,0 +1,10 @@
+export interface FoodItem {
+    id?: number;
+    item?: string;
+    itemDescription?: string;
+    isVeg?: boolean;
+    price?: number;
+    restaurantId?: number;
+    quantity: number;
+}
+
